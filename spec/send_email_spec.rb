051107a09e67d4e0c0adbef3ad15563c9e8d5e2a
Margo Flewelling <flewelling.margo@gmail.com>
2020-07-19 21:42:51 -0600

    Add test body and simple test, need to add test to check status and message

--- spec/send_email_spec.rb
+++ spec/send_email_spec.rb
@@ -3,22 +3,43 @@ require 'spec_helper'
 
 describe "My Sinatra Application" do
   include Rack::Test::Methods
-  def app
-    EmailService.new
-  end
 
-  it "test testing env" do
-    get '/'
-    expect(last_response).to be_ok
-  end
+    def app
+      EmailService.new
+    end
 
-  # it "should receive a post message and send an email to the emergency contact" do
-  # # json = post body
-  #   post '/email_alert', :data => json
-  #   expect(last_response).to be_ok
-  #   info = JSON.parse(last_response.body)
-  #   expect(info.is_a? Hash).to eq(true)
-  #   # expect(items.first["attributes"].has_key?("coordinates")).to eq(true)
-  # end
+    it "should receive a post message and send an email to the emergency contact" do
+      info = { data:
+             { user: { name: "John Doe",
+                       email: "john@example.com",
+                       phone: "222 224 2222",
+                       address: "123 hifu ln, denver co, 80210",
+                       age: 33,
+                       race: "white",
+                       gender: "male",
+                       sat_tracker_address: "1242354235",
+                       blood_type: "O neg",
+                       allergies: "peanuts",
+                       medical_conditions: "none",
+                       heightCM: 183,
+                       weightKG: 73,
+                       contact: { name: "Jane Doe",
+                                  email: "flewelling.margo@gmail.com",
+                                  phone: "233 333 3333" },
+                       route: { start_time: "July 20 2020, 5 pm",
+                                end_time: "July 27 2020, 7pm",
+                                activity: "backpacking",
+                                party_size: 1,
+                                notes: "backpacking and fishing in mt zirkel wilderness" }
+                             # need to add waypoints here}
+                       }
+                  }
+                }
+        post '/email_alert', info.to_json
+        expect(last_response).to be_ok
+
+        # info = JSON.parse(last_response.body)
+        # expect(info.is_a? Hash).to eq(true)
+      end
 
 end
