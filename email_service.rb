ef5a86602b84b897bc25f3685b6b9d2bedb26883
Margo Flewelling <flewelling.margo@gmail.com>
2020-07-19 21:39:15 -0600

    Create info object and send email through service

--- email_service.rb
+++ email_service.rb
@@ -5,6 +5,7 @@ require 'json'
 require './services/sendgrid_service'
 require './poros/info'
 
+
 class EmailService < Sinatra::Base
 
   get "/" do
@@ -12,13 +13,16 @@ class EmailService < Sinatra::Base
   end
 
   post "/email_alert" do
-    # request.body.read
-    # take in params/body and create info object to populate form
-    # info = Info.new(request.body)
-    # SendGridService.new.send_email(info)
-    # send email via sendgrid service
-    require "pry"; binding.pry
+    request_body = JSON.parse(request.body.string)
+    info = Info.new(request_body)
+    SendGridService.new.send_email(info)
+    # require "pry"; binding.pry
+    # if info.save
+    #   require "pry"; binding.pry
+    #   render :status => :created, json: { message: "Email send to #{info.contact_name}"}
+    # else
+    #   message = user.errors.full_messages.to_sentence
+    #   render :status => 400, json: { message: message }
+    # end
   end
 end
-
-# run EmailService.run!
