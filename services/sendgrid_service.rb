84ae55b4b701fb67cfda587434510ee8ac206996
Margo Flewelling <flewelling.margo@gmail.com>
2020-07-19 21:42:05 -0600

    Load env variables

--- services/sendgrid_service.rb
+++ services/sendgrid_service.rb
@@ -1,7 +1,9 @@
-
 # using SendGrid's Ruby Library
 # https://github.com/sendgrid/sendgrid-ruby
 require 'sendgrid-ruby'
+require 'dotenv'
+Dotenv.load
+
 include SendGrid
 
 class SendGridService
@@ -9,10 +11,9 @@ class SendGridService
   def send_email(info)
     from = Email.new(email: 'hifudev2001@gmail.com')
     to = Email.new(email: info.contact_email)
-    subject = "Emergency contact alert regarding #{info.person_name}"
+    subject = "Emergency Alert Regarding #{info.person_name}"
     content = Content.new(type: 'text/plain', value: info.message)
     mail = Mail.new(from, subject, to, content)
-
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
     puts response.status_code
