cf254a68600f2c9f6e677126f12e08b4a0408c5c
Margo Flewelling <flewelling.margo@gmail.com>
2020-07-25 22:55:25 -0600

    Remove pry

--- services/sendgrid_service.rb
+++ services/sendgrid_service.rb
@@ -45,7 +45,6 @@ class SendGridService
     },
     template_id: "d-ad4aeb03a37c4b78970df5d14ff6bd22"
   }
-  require "pry"; binding.pry
   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
   response = sg.client.mail._('send').post(request_body: data)
   response
